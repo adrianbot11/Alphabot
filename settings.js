444e71666e21e4566508b0b6bf983491c0869ae8
Riann <99214938+adrianbot11@users.noreply.github.com>
2022-10-21 11:18:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Kenzo-Botz" //namabot kalian
-global.ownername= "ᴹᴿ᭄ Adam ོ ×፝֟͜×" //nama kalian
+global.botname = "Riann-Botz" //namabot kalian
+global.ownername= "ᴹᴿ᭄ Rian Owner ོ ×፝֟͜×" //nama kalian
 global.myweb ="-" //bebas asal jan hapus
 global.youtube = "-" //bebas asal jan hapus
 global.myweb2 = "-" //bebas
 global.email = "-" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6281214557752'] //premium user
-global.owner = ["6281214557752","6281214557752"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281214557752" // nomor wa kalian
-global.ownernomerr = "6281214557752" //nmr wa kalian
+global.premium = ['6289613336819'] //premium user
+global.owner = ["6289613336819","6289613336819"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289613336819" // nomor wa kalian
+global.ownernomerr = "6289613336819" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Kenzo-Botz ོ' //sticker wm ubah
-global.author = 'Di Buat Oleh Adam' //sticker wm ganti nama kalian
+global.packname = '© Rian-Botz ོ' //sticker wm Rian Md
+global.author = 'Di Buat Oleh Rian' //sticker wm Rian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
